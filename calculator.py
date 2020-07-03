ed7048912153ee72a940ad7b1650478f1f539b61
kssanthosh <67405923+kssanthosh-ges@users.noreply.github.com>
2020-07-03 11:24:37 +0530

    Create calculator.py
    Adding base math file to the master with initial commit

---
+++ calculator.py
@@ -0,0 +1,18 @@
+"""
+This file to get the basic math operation on the given inputs to it
+"""
+
+def add(n1,n2):
+    pass
+
+def sub(n1,n2):
+    pass
+
+def mul(n1,n2):
+    pass
+   
+def div(n1,n2):
+    pass
+
+def mod(n1,n2):
+    pass
